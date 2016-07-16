c25288edfd562cb6e5d867ffde5a406cbc8f22ba
Arnaud Balma <delwende.arnaud@gmail.com>
2016-07-16 08:53:30 -0500

    Template refactoring + compression module

--- assybeauty/settings.py
+++ assybeauty/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = (
     'bootstrap3',
     'bootstrap_themes',
     'registration',
+    'compressor',
     'store',
 )
 
@@ -109,6 +110,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+COMPRESS_ENABLED = True
+STATICFILES_FINDERS=(
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+)
 
 # Registration
 ACCOUNT_ACTIVATION_DAYS = 7
